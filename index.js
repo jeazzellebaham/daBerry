664b7bbd62d3e5c7579806e2db4e8fc721915a65
jeazzellebaham <24.lillianna@gmail.com>
2020-10-01 23:24:38 +0000

    * add makeBody method

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 (function(window, _) {
   window.myLibraryName = window.myLibraryName || {
-    numz: {},
+    numz: {
+
+     getAngleDegrees (pointA, pointB) {
+            const
+            distanceX = pointB.x - pointA.x,
+            distanceY = pointB.y - pointA.y,
+            radians = Math.atan2(distancY, distanceX),
+            degrees = radians * 180 / Math.PI;
+                return degrees;
+        },
+    degreesToRadians (degrees) {
+        return degrees * Math.PI / 180;
+        },
+    radiansToDgrees (radians) {
+        return radians * 180 / Math.PI;
+    }
+    },
     phyz: {},
   };
 }(window, window._));
